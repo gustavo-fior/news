d35c697658f021182cc7cfb36e68318b8639f3c8
Gustavo Fior <gustavo_fior@outlook.com>
2022-02-14 10:23:18 -0300

    Arrumando CORS Policy

--- src/main/java/br/com/gx/news/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/gx/news/config/security/SecurityConfigurations.java
@@ -64,7 +64,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter impleme
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT",
+		registry.addMapping("/**").allowedOrigins("https://brazilnews.herokuapp.com").allowedMethods("GET", "POST", "PUT",
 				"DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 
